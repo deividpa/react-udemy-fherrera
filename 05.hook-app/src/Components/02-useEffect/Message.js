09f53a47837d4214c67fcfb7c50cbbd914e2c93d
David Pérez <deividpa022@gmail.com>
2022-12-07 11:58:28 -0500

    Se hace uso de la cleanup function del useEffect para remover un listener al mover el mouse, haciendo un correcto unmount del effect

--- 05.hook-app/src/Components/02-useEffect/Message.js
+++ 05.hook-app/src/Components/02-useEffect/Message.js
@@ -1,17 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+  const { x, y } = coords;
+
   useEffect(() => {
     console.log("Message mounted");
 
+    const mouseMove = (e) => {
+      const coords = { x: e.x, y: e.y };
+      setCoords(coords);
+      console.log(":D");
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
     return () => {
       console.log("Message unmounted");
+      window.removeEventListener("mousemove", mouseMove);
     };
   }, []);
 
   return (
     <>
-      <h2>Message</h2>
+      <h2>
+        Message with coords: x:{x} - y:{y}
+      </h2>
     </>
   );
 };
